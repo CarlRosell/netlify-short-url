7f07e64703d3b25c7b6ef76d21a4de08bf843ea0
Carl Rosell <me@carlrosell.com>
2018-12-06 22:44:05 +0100

    Update config.yml

--- buildRedirect.js
+++ buildRedirect.js
@@ -3,8 +3,8 @@ const { urls } = require("./urls.json");
 
 const redirectContent = urls.map(a => `/${a.code} ${a.url}`).join("\n");
 
-fs.writeFile("public/_redirects", redirectContent, function(err) {
+fs.writeFile("public/_redirects", redirectContent, err => {
   // If an error occurred, show it and return
   if (err) return console.error(err);
-  // Successfully wrote binary contents to the file!
+  // Successfully wrote contents to the file!
 });
